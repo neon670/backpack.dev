c151cd5db854da99993c3ff61ccadd28073a32f1
Noe Nevarez <closner_nevarez@hotmail.com>
2015-05-09 17:39:22 -0500

    first commit

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://backpack.dev/admin/');
+define('HTTP_CATALOG', 'http://backpack.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://backpack.dev/admin/');
+define('HTTPS_CATALOG', 'http://backpack.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/vagrant/sites/backpack.dev/public/admin/');
+define('DIR_SYSTEM', '/vagrant/sites/backpack.dev/public/system/');
+define('DIR_LANGUAGE', '/vagrant/sites/backpack.dev/public/admin/language/');
+define('DIR_TEMPLATE', '/vagrant/sites/backpack.dev/public/admin/view/template/');
+define('DIR_CONFIG', '/vagrant/sites/backpack.dev/public/system/config/');
+define('DIR_IMAGE', '/vagrant/sites/backpack.dev/public/image/');
+define('DIR_CACHE', '/vagrant/sites/backpack.dev/public/system/cache/');
+define('DIR_DOWNLOAD', '/vagrant/sites/backpack.dev/public/system/download/');
+define('DIR_UPLOAD', '/vagrant/sites/backpack.dev/public/system/upload/');
+define('DIR_LOGS', '/vagrant/sites/backpack.dev/public/system/logs/');
+define('DIR_MODIFICATION', '/vagrant/sites/backpack.dev/public/system/modification/');
+define('DIR_CATALOG', '/vagrant/sites/backpack.dev/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'backpack');
+define('DB_PASSWORD', 'backpack');
+define('DB_DATABASE', 'backpack_db');
+define('DB_PREFIX', 'oc_');
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://backpack.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://backpack.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/vagrant/sites/backpack.dev/public/catalog/');
+define('DIR_SYSTEM', '/vagrant/sites/backpack.dev/public/system/');
+define('DIR_LANGUAGE', '/vagrant/sites/backpack.dev/public/catalog/language/');
+define('DIR_TEMPLATE', '/vagrant/sites/backpack.dev/public/catalog/view/theme/');
+define('DIR_CONFIG', '/vagrant/sites/backpack.dev/public/system/config/');
+define('DIR_IMAGE', '/vagrant/sites/backpack.dev/public/image/');
+define('DIR_CACHE', '/vagrant/sites/backpack.dev/public/system/cache/');
+define('DIR_DOWNLOAD', '/vagrant/sites/backpack.dev/public/system/download/');
+define('DIR_UPLOAD', '/vagrant/sites/backpack.dev/public/system/upload/');
+define('DIR_MODIFICATION', '/vagrant/sites/backpack.dev/public/system/modification/');
+define('DIR_LOGS', '/vagrant/sites/backpack.dev/public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'backpack');
+define('DB_PASSWORD', 'backpack');
+define('DB_DATABASE', 'backpack_db');
+define('DB_PREFIX', 'oc_');
